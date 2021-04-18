6f383744dfa665be7a8f27f418b283e0c6b56ad5
slingshot-dev <slingshot@outlook.fr>
2021-04-18 19:18:10 +0200

    Start Projet Notes

---
+++ src/test/java/com/example/MediscreenNotes/MediscreenNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MediscreenNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
